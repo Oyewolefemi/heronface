6dfa2640b8831c81dbb1239dc7a587396d44ca30
Oyewolefemi <111422526+Oyewolefemi@users.noreply.github.com>
2026-05-07 12:42:51 +0100

    Delete app directory

--- app/Models/MarketSignal.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class MarketSignal extends Model {
-    protected $fillable = ['brain_cycle_id', 'title', 'source', 'url', 'explained_content', 'color_tag'];
-}
--- app/Models/VendorHook.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class VendorHook extends Model {
-    protected $fillable = ['brain_cycle_id', 'vendor_name', 'hook_text', 'status'];
-}
